c25e27f36606d05d3cfa6e3abb468fc90a2fd4cb
Adrian Popa <adrian.popa.gh@gmail.com>
2019-06-25 16:05:53 +0300

    Update probes, youtube-dl on startup

---
+++ init/80_update_probes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+logger -s -t $0 "Updating probes..."
+curl -L -o /tmp/speedtest.pm https://github.com/mad-ady/smokeping-speedtest/raw/master/speedtest.pm
+curl -L -o /tmp/YoutubeDL.pm https://github.com/mad-ady/smokeping-youtube-dl/raw/master/YoutubeDL.pm
+curl -L -o /tmp/WifiParam.pm https://github.com/mad-ady/smokeping-wifi-param/raw/master/WifiParam.pm
+
+for file in speedtest.pm YoutubeDL.pm WifiParam.pm;
+do
+    if [ -s "/tmp/$file" ]; then
+        mv "/tmp/$file" "/usr/share/perl5/Smokeping/probes/$file"
+    fi
+done
+
+logger -s -t $0 "Updating youtube-dl..."
+curl -L -o /tmp/youtube-dl https://yt-dl.org/downloads/latest/youtube-dl
+
+for file in youtube-dl;
+do
+    if [ -s "/tmp/$file" ]; then
+        mv "/tmp/$file" "/usr/local/bin/$file"
+        chmod a+x /usr/local/bin/$file
+    fi
+done
